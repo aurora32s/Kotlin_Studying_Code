0bc638a437bff7506d1140cc2849609f952edbee
Round-PC\Round <aurora32s@naver.com>
2019-07-21 20:40:21 +0900

    no message

--- Smart_Penalty/app/src/main/java/com/example/round/smart_penalty/CrimeActivity.kt
+++
@@ -1,9 +0,0 @@
-package com.example.round.smart_penalty
-
-import android.support.v7.app.AppCompatActivity
-
-/**
- * Created by Round on 2017-09-18.
- */
-class CrimeActivity : AppCompatActivity() {
-}
--- jejuFarm (2)/app/src/main/java/kr/co/infomind/app/farmsns/DroidGap.kt
+++
@@ -1,9 +0,0 @@
-package kr.co.infomind.app.farmsns
-
-import android.app.Activity
-
-/**
- * Created by itnote on 2017. 3. 21..
- */
-
-open class DroidGap : Activity()
